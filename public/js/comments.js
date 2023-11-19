c5dbf9b9a403aff9cdc233ce3dc7709b0a115343
Pam <pwillisop@yahoo.com>
2023-11-19 18:32:59 -0500

    need help directing commit

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,35 +1,37 @@
 const newCommentHandler = async (event) => {
-    try {
+  try {
+    event.preventDefault();
+
     const comments = document.querySelector('#comments-remark').value.trim();
 
     const commentData = {
-        remark: remark
+      remark: remark
     };
 
 
-    const response = await fetch(`/api/blogs/${blogID}`, {
-        method: 'POST',
-        body: JSON.stringify(commentData),
-        remark: {
-            'Content-Type': 'application/json',
-        },
+    const response = await fetch(`/api/comments/${blogID}`, {
+      method: 'POST',
+      body: JSON.stringify(commentData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
 
     if (response.ok) {
-        console.log('Comment post created successfully:', commentData);
-        document.location.replace('/');
-      } else {
-        console.error('Failed to create comment:', response.status, response.statusText);
-        alert('Failed to create comment');
-      }
-    } catch (error) {
-      console.error('Error in newCommentHandler:', error);
-      alert('An error occurred while processing the form');
+      console.log('Comment post created successfully:', commentData);
+      document.location.replace('/blog/:id');
+    } else {
+      console.error('Failed to create comment:', response.status, response.statusText);
+      alert('Failed to create comment');
     }
-  };
+  } catch (error) {
+    console.error('Error in newCommentHandler:', error);
+    alert('An error occurred while processing the form');
+  }
+};
 
-  document
+document
   .querySelector('.new-comment-form')
   .addEventListener('submit', newCommentHandler);
 
