b167682926f98b3a90e843f9a40608ae02df05da
Pam <pwillisop@yahoo.com>
2023-11-21 18:55:02 -0500

    shift change

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -3,18 +3,17 @@ const router = express.Router();
 const { User, Blog, Comments } = require('../../models')
 const withAuth = require('../../utils/auth')
 
-router.post('/comments', withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     // Get the comment data from the request body
-    const { remark, blogId } = req.body;
-
+    // const { remark, blogId } = req.body;
+console.log(req)
     // Create the comment in the database
     const newComment = await Comments.create({
-      remark: remark,
-      blog_id: blogId,
+      ...req.body,
       user_id: req.session.user_id,
     });
-
+console.log(newComment)
     // Redirect the user back to the blog page
     res.redirect(`/blogs/${blogId}`);
   } catch (err) {
--- public/js/comments.js
+++ public/js/comments.js
@@ -5,26 +5,28 @@ commentForm.addEventListener('submit', async (e) => {
   // Get the comment text and blog ID from the form
   const remark = commentForm.querySelector('textarea[name="remark"]').value;
   const blogId = commentForm.querySelector('input[name="blogId"]').value;
-  console.log(remark)
+
   try {
     // Send a POST request to the server to add the comment
-    const response = await fetch('/comments', {
+    const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({ remark, blogId }),
       headers: { 'Content-Type': 'application/json' },
     });
-    
+console.log(response)
     if (response.ok) {
       // Refresh the page to show the new comment
-      location.reload();
+      document.location.reload();
     } else {
+
       // Handle any errors with an alert
-      alert('Failed to add comment');
+      alert('Failed to add thiscomment');
     }
   } catch (err) {
     // Handle any errors with an alert
-    alert('Failed to add comment');
+    alert('Failed to add mycomment');
     console.log(err)
+
   }
 });
 
